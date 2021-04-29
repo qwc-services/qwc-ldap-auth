f0ed5ff2cce1ae08bf3ece741a4daff432c9c011
Pirmin Kalberer <pka@sourcepole.ch>
2021-04-29 13:59:04 +0200

    Add config vars for cookie settings

--- server.py
+++ server.py
@@ -22,6 +22,13 @@ from qwc_services_core.tenant_handler import (
 
 app = Flask(__name__)
 
+app.config['JWT_COOKIE_SECURE'] = bool(os.environ.get(
+    'JWT_COOKIE_SECURE', False))
+app.config['JWT_COOKIE_SAMESITE'] = os.environ.get(
+    'JWT_COOKIE_SAMESITE', 'Lax')
+app.config['JWT_ACCESS_TOKEN_EXPIRES'] = int(os.environ.get(
+    'JWT_ACCESS_TOKEN_EXPIRES', 12*3600))
+
 jwt = jwt_manager(app)
 app.secret_key = app.config['JWT_SECRET_KEY']
 

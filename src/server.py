9bc76fb46290eea17fcb7f4ae45365b7a590f33b
Sandro Mani <manisandro@gmail.com>
2026-02-06 22:54:23 +0100

    Allow setting local webserver via FLASK_RUN_PORT

--- src/server.py
+++ src/server.py
@@ -329,5 +329,6 @@ def url_path(url):
 
 
 if __name__ == '__main__':
+    print("Starting LDAP service...")
     app.logger.setLevel(logging.DEBUG)
-    app.run(host='localhost', port=5017, debug=True)
+    app.run(host='localhost', port=os.environ.get("FLASK_RUN_PORT", 5000), debug=True)
